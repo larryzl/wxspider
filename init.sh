a810dfacd82ec77c8d0b584c00de7e9237895d29
zhanglei <zhanglei@tansuotv.com>
2019-08-21 17:53:05 +0800

    更新pip 包列表

--- init.sh
+++ init.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# 删除migrations文件
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-# 删除__pycache__文件
-find . -path "*/__pycache__/*" -delete
+pip install -r pip_require.txt
 
-# 删除数据库文件
-rm -f db.sqlite3
-
-# 生成数据库文件
-#python manage.py makemigrations
-#python manage.py migrate
